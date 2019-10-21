e5c7392031b58c8f517e9085bd95341177c27f5b
Angel <mac@macbook.local>
2019-10-21 19:09:52 -0300

    Clase 12 - Actividad de Clase

---
+++ Clase 12/ActividadClase.sql
@@ -0,0 +1,15 @@
+-- Hacer un procedimiento almacenado que permita inscribir un socio a una
+-- actividad. Se debe recibir el Legajo, el código de actividad y el estado
+-- de beca. El procedimiento debe asegurar que un socio no pueda estar becado
+-- en más de dos actividades a la vez. En caso de que esto ocurra, listar un
+-- mensaje de error y no registrar la inscripción. De lo contrario, inscribir al socio
+
+
+-- Crear un Trigger que al eliminar un socio, 
+-- elimine toda la información de inscripciones
+-- y pagos realizados.
+
+
+-- Crear un Trigger que al agregar un pago, establezca el importe del mismo a partir
+-- del costo de las actividades que el socio realiza.
+
